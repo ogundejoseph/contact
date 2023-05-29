15300f3f3d61688851b5f37e0ab2463880ee0a95
ogundejoseph <127089996+ogundejoseph@users.noreply.github.com>
2023-05-29 09:56:37 +0300

    initial commit

---
+++ mail.php
@@ -0,0 +1,31 @@
+<?php
+    $sEmail = $_POST['sEmail'];
+    $rsEmail = [$_POST['rEmail']];
+    $name = $_POST['name'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    if(!empty($sEmail) && !empty($rEmail) && !empty($message)) {
+        if(empty($subject)) {
+            $subject = "(no subject)";
+        }
+        if(!filter_var($sEmail, FILTER_VALIDATE_EMAIL)) {
+            echo "Invalid sender's email!";
+        }
+        if(filter_var($rEmail, FILTER_VALIDATE_EMAIL)) {
+            //$sender = "From: $sEmail";
+            //$sender = "$name <$sEmail>";
+            $to = "$rsEmail";
+            $headers = 'From: <$sEmail>' . "\r\n";
+            if(mail($to, $subject, $message, $headers)) {
+                echo "Message sent";
+            } else {
+                echo "Failed to send your message!";
+            }
+        } else {
+            echo "Invalid recepient's email!";
+        }
+    } else {
+        echo "Email and message field required!";
+    }
+?>
